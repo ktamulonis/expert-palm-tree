577d6424339c2f002db4fc81d427346019db673e
ktamulonis <kurttamulonis@gmail.com>
2019-02-16 21:11:51 -0500

    Tests post fixture

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "post_count" do
+     assert_equal 2, Post.count
+   end
 end
